0eb7b0f6aa80552191162a474f0824ebb6ba6184
GoodGuyForReal <tdoganay325@gmai.com>
2023-05-02 12:17:42 +0200

    navbar design updates

---
+++ app/doc/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>Doc</div>
+    )
+}
+
+export default page
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,5 @@
+import Navbar from './components/navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +9,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={''}>
+        <Navbar />
         {children}
       </body>
     </html>
