8dd9c67141c8bf448c72d9bb10d86757d631abf5
GoodGuyForReal <tdoganay325@gmai.com>
2023-05-03 13:46:14 +0200

    sidebar updated sub tabs added

--- app/doc/components/Sidebar.jsx
+++ app/doc/components/Sidebar.jsx
@@ -1,22 +1,37 @@
 import React from 'react'
 
-
-
-const SideBar = ({ tabs, setActiveTab, activeTab }) => {
+const SideBar = ({ tabs, setActiveTab, activeTab, setActiveSubTab, activeSubTab }) => {
     return (
         <div className="flex flex-col sidebar px-10 pt-5">
             {tabs.map((tab, index) => (
-                <button
-                    key={index}
-                    className={`flex items-center gap-2 py-2 px-4 text-sm text-left text-light-secondary font-medium hover:underline duration-200${activeTab === index
-                        ? " text-white bg-gray-900 rounded-lg"
-                        : "text-light-secondary"
-                        }`}
-                    onClick={() => setActiveTab(index)}
-                >
-                    {tab.icon}
-                    {tab.title}
-                </button>
+                <div key={index}>
+                    <button
+                        className={`flex items-center gap-2 py-2 px-4 text-sm text-left text-light-secondary font-medium hover:underline duration-200${activeTab === index
+                            ? " text-white bg-gray-900 rounded-lg w-full"
+                            : "text-light-secondary"
+                            }`}
+                        onClick={() => setActiveTab(index)}
+                    >
+                        {tab.icon}
+                        {tab.title}
+                    </button>
+                    {tab.subtabs && activeTab === index && (
+                        <div className="ml-3 mt-2">
+                            {tab.subtabs.map((subtab, subIndex) => (
+                                <button
+                                    key={subIndex}
+                                    className={`flex items-center gap-2 py-1 px-4 text-sm text-left text-light-secondary font-medium hover:underline duration-200${activeSubTab === subIndex
+                                        ? " text-white bg-gray-900 rounded-lg w-full"
+                                        : "text-light-secondary"
+                                        }`}
+                                    onClick={() => setActiveSubTab(subIndex)}
+                                >
+                                    {subtab.title}
+                                </button>
+                            ))}
+                        </div>
+                    )}
+                </div>
             ))}
         </div>
     );
--- app/doc/page.jsx
+++ app/doc/page.jsx
@@ -5,11 +5,14 @@ import SideBar from './components/Sidebar';
 import Intro from './components/sections/intro/Intro.jsx';
 import Usage from './components/sections/usage/Usage.jsx';
 
-import { HomeIcon, DocumentCheckIcon } from '@heroicons/react/24/outline'
+import { HomeIcon, CloudArrowDownIcon } from '@heroicons/react/24/outline'
+import Buttons from './components/sections/components/buttons/Buttons';
+import Inputs from './components/sections/components/inputs/Inputs';
 
 
 const page = () => {
     const [activeTab, setActiveTab] = useState(0);
+    const [activeSubTab, setActiveSubTab] = useState(0);
     const tabs = [
         {
             title: "Getting Started",
@@ -19,23 +22,38 @@ const page = () => {
         {
             title: "Installation",
             content: <Usage />,
-            icon: <DocumentCheckIcon className="h-5 w-h-5" />
+            icon: <CloudArrowDownIcon className="h-5 w-h-5" />
         },
         {
             title: "Components",
-            content: "Examples",
+            subtabs: [
+                {
+                    title: "Buttons",
+                    component: <Buttons />
+                },
+                {
+                    title: "Inputs",
+                    component: <Inputs />
+                }
+                // add more subtabs here as needed
+            ]
         },
+
     ];
 
 
     return (
-        <div className="w-full h-full pt-12 flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10 bg-slate-950">
+        <div className="w-full h-full pt-12 flex-1 items-start md:grid md:grid-cols-[300px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[350px_minmax(0,1fr)] lg:gap-10 bg-slate-950">
+
             <div className="fixed top-14 z-30 -ml-2 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r md:sticky md:block">
-                <SideBar tabs={tabs} setActiveTab={setActiveTab} activeTab={activeTab} />
+                <SideBar tabs={tabs} setActiveTab={setActiveTab} activeTab={activeTab} setActiveSubTab={setActiveSubTab} activeSubTab={activeSubTab} />
             </div>
 
             <main className="relative py-6 lg:gap-10 lg:py-8 xl:grid xl:grid-cols-[1fr_300px]">
-                {tabs[activeTab].content}
+                {tabs[activeTab].subtabs
+                    ? tabs[activeTab].subtabs[activeSubTab].component
+                    : tabs[activeTab].content
+                }
             </main>
         </div>
     );
