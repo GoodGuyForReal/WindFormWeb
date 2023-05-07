a037d5ab036608f5062ed2e99b5697dce8c4bb69
GoodGuyForReal <tdoganay325@gmai.com>
2023-05-07 15:56:23 +0200

    multi select input updated

--- app/doc/components/sections/components/inputs/inputMultiSelect/InputMultiSelect.jsx
+++ app/doc/components/sections/components/inputs/inputMultiSelect/InputMultiSelect.jsx
@@ -3,23 +3,27 @@ import React, { useState, useRef } from 'react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 
 const team = [
-    { value: 'Kaven', label: 'Kaven' },
-    { value: 'Jane', label: 'Jane' },
-    { value: 'Jason', label: 'Jason' },
-    { value: 'Karen', label: 'Karen' },
+    { empID: 1, value: 'Jane', label: 'Jane' },
+    { empID: 2, value: 'Jason', label: 'Jason' },
+    { empID: 3, value: 'Karen', label: 'Karen' },
 ];
 
 const InputMultiSelect = () => {
-    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [selectedOptions, setSelectedOptions] = useState([{ empID: 4, value: 'Kaven', label: 'Kaven' },]);
     const [isSelectOpen, setIsSelectOpen] = useState(false)
     const [searchitem, setSearchitem] = useState('')
     const wrapperRef = useRef(null);
 
     popUpCloser(wrapperRef, setIsSelectOpen);
 
-    const handleSelect = (item) => setSelectedOptions(current => [item, ...current])
+    const handleSelect = (item) => {
+        setSelectedOptions(current => [item, ...current])
+        team.filter((item) => item.empID !== item)
+    }
 
-    const handleSelectRemove = (memberID) => team.filter(item => item !== memberID)
+    const handleSelectRemove = (memberID) => setSelectedOptions((current) =>
+        current.filter((item) => item.empID !== memberID)
+    );
 
     const search = searchitem.trim() !== ''
         ?
@@ -36,40 +40,41 @@ const InputMultiSelect = () => {
                 <div
                     name="person"
                     id="person"
-                    className="flex  w-full rounded-md outline-none border-0 h-full min-h-[40px] py-2 px-2 bg-slate-50 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6"
-                    onClick={() => setIsSelectOpen(true)}
+                    className="flex w-full rounded-md outline-none border-0 h-full min-h-[40px] py-2 px-2 bg-slate-50 text-gray-900 ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6"
+
                 >
-                    <div className='team_main flex flex-wrap gap-2'>
+                    <div className='team_main w-full flex flex-wrap items-center gap-2'>
                         {
                             selectedOptions.map((item, id) => (
                                 <span
                                     key={id}
-                                    className='py-1 px-3 flex items-center gap-2 font-medium text-sm rounded-full border border-gray-300 hover:bg-slate-100 text-gray-800 '
-                                    onClick={() => handleSelectRemove(id)}
+                                    className='py-1 px-3 flex items-center gap-2 font-medium text-sm rounded-full border border-gray-300 hover:bg-red-100 hover:border-red-300 duration-200 text-gray-800 cursor-default'
+                                    onClick={() => handleSelectRemove(item.empID)}
                                 >
-                                    {item}
+                                    {item.label}
                                     <XMarkIcon className='h-4 w-4 text-gray-800' />
                                 </span>
                             ))
                         }
+                        <input
+                            type="text"
+                            className='w-[50%] bg-transparent outline-none text-sm text-gray-800 p-1'
+                            placeholder='Search...'
+                            onChange={(e) => setSearchitem(e.target.value)}
+                            onClick={() => setIsSelectOpen(true)}
+                        />
                     </div>
                 </div>
                 <div className='relative w-full h-full'>
                     {isSelectOpen &&
                         <div ref={wrapperRef} className="absolute z-10 top-0 left-0 right-20 items-center drop-shadow-sm">
-                            <input
-                                type="text"
-                                className='w-full py-2 px-4 bg-slate-100/20 border text-sm text-gray-800 rounded-t-md '
-                                placeholder='Search person'
-                                onChange={(e) => setSearchitem(e.target.value)}
-                            />
                             <div className='drop_down w-full border rounded-b-md'>
                                 {search.map((option) => (
                                     <p
-                                        onClick={() => handleSelect(option.value)}
+                                        onClick={() => handleSelect(option)}
                                         key={option.value}
                                         value={option.value}
-                                        className='text-gray-800 w-full py-2 px-2 bg-slate-50 even:bg-slate-100/20'
+                                        className='text-gray-800 w-full py-2 px-2 bg-slate-50 even:bg-slate-100/20 cursor-default truncate'
                                     >
                                         {option.label}
                                     </p>
