dfdc7a01bb1f6ff52cbe1d30b14d7fe48b6cccce
GoodGuyForReal <tdoganay325@gmai.com>
2023-05-27 20:58:38 +0200

    ComponenetLinks is updated

--- app/doc/components/ComponentLinks.jsx
+++ app/doc/components/ComponentLinks.jsx
@@ -4,7 +4,17 @@ import Link from "next/link";
 
 
 const ComponentLinks = ({ components }) => {
+  const handleClick = (event, id) => {
+    event.preventDefault();
 
+    const sectionElement = document.getElementById(id);
+    const offset = 100; 
+
+    if (sectionElement) {
+      const topOffset = sectionElement.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({ top: topOffset - offset, behavior: 'smooth' });
+    }
+  };
 
   return (
    <div className="included_main ml-2 mt-5 flex flex-col gap-2">
@@ -14,6 +24,7 @@ const ComponentLinks = ({ components }) => {
               scroll={false}
               className="cursor-pointer text-sm text-gray-400 duration-200 hover:text-gray-800"
               key={id}
+              onClick={(event) => handleClick(event, item.href)}
             >
               {item.name}
             </Link>
