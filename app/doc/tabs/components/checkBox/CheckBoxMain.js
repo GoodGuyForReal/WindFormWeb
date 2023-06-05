5483a89803a11c24030189012fe7fdb77f18fe43
GoodGuyForReal <tdoganay325@gmai.com>
2023-06-05 09:22:54 +0200

    check outline added

--- app/doc/tabs/components/checkBox/CheckBoxMain.js
+++ app/doc/tabs/components/checkBox/CheckBoxMain.js
@@ -1,31 +1,36 @@
 import CheckDefaultTS from "./checkDefault/CheckDefaultTS";
 import CheckDisableTS from "./checkDisable/CheckDisableTS";
+import CheckOutlineTS from "./checkOutline/CheckOutlineTS";
 import CheckWithLabelTS from "./checkWithLabel/CheckWithLabelTS";
 
-
 export const CheckBoxMain = [
-    {
-        title: 'Check Box',
-        subComponent: [
-            {
-                name: 'Input Checkbox Default',
-                component: <CheckDefaultTS />,
-                code: ``,
-                href: 'InputCheckboxDefault'
-            },
-            {
-                name: 'Input Check With Label',
-                component: <CheckWithLabelTS />,
-                code: ``,
-                href: 'InputCheckWithLabel'
-            },
-            {
-                name: 'Input Check Disable',
-                component: <CheckDisableTS />,
-                code: ``,
-                href: 'InputCheckDisable'
-            },
-        ]
-        
-    }
-]
+  {
+    title: "Check Box",
+    subComponent: [
+      {
+        name: "Input Checkbox Default",
+        component: <CheckDefaultTS />,
+        code: ``,
+        href: "InputCheckboxDefault",
+      },
+      {
+        name: "Input Check Outline",
+        component: <CheckOutlineTS />,
+        code: ``,
+        href: "CheckOutline",
+      },
+      {
+        name: "Input Check With Label",
+        component: <CheckWithLabelTS />,
+        code: ``,
+        href: "InputCheckWithLabel",
+      },
+      {
+        name: "Input Check Disable",
+        component: <CheckDisableTS />,
+        code: ``,
+        href: "InputCheckDisable",
+      },
+    ],
+  },
+];
