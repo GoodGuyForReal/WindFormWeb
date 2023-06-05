5ff7f02b368f80d257a11a62d551a726f8cf02e9
GoodGuyForReal <tdoganay325@gmai.com>
2023-06-05 10:28:50 +0200

    searchButtonOutside py updated

--- app/doc/tabs/components/inputs/inputButtonOutside/InputButtonOutside.tsx
+++ app/doc/tabs/components/inputs/inputButtonOutside/InputButtonOutside.tsx
@@ -15,7 +15,7 @@ const InputButtonOutside = () => {
           type="text"
           name="searchButtonOutside"
           id="searchButtonOutside"
-          className="block w-full rounded-md border-0 py-2.5 pl-4 pr-[88px] text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
+          className="block w-full rounded-md border-0 py-2 pl-4 pr-[88px] text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-500 sm:text-sm sm:leading-6"
           placeholder="Search..."
         />
         <button className="rounded-md bg-blue-500 px-6 py-2 font-medium text-sm text-slate-50 duration-200 hover:bg-blue-500/80">
