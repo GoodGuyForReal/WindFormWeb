de300e874f60ba2fa5d4e88d51585fa698d514f9
GoodGuyForReal <tdoganay325@gmai.com>
2023-06-11 21:36:39 +0200

    hero added

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Pricing from "../components/sections/Pricing";
 import ShowCase from "../components/sections/ShowCase";
+import Hero from "../components/sections/Hero";
 
 export default function Home() {
   return (
     <main className="mx-auto flex w-full max-w-7xl flex-col items-center px-6 lg:px-8">
+      <Hero />
       <ShowCase />
       <Pricing />
     </main>
